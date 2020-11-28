2f3e0c49b396389c33c22af0309d1867479dca35
Rocco Bruyn <rocco.bruyn@gmail.com>
2020-11-28 19:29:57 +0100

    goodbye world

--- src/hello-world/index.ts
+++
@@ -1,7 +0,0 @@
-import { html } from 'lit-html';
-import { hello, world } from './hello-world.module.scss';
-
-export default () => html`
-<h1 class="${hello}">Hello World</h1>
-<p class="${world}">Foo bar baz</p>
-`;
